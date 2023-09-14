2e012ebf3cf24243b2f2e824c22511870a1bae09
lomi95 <67117720+lomi95@users.noreply.github.com>
2023-09-14 15:45:10 +0200

    Update analysis_cardio.R

--- analysis_cardio.R
+++ analysis_cardio.R
@@ -24,24 +24,3 @@ names(all_excels) <- all_excels
 
 cytoscapePing()
 mm <- lapply(all_excels,Load2Cytoscape, 0.6)
-
-SUID_list <- getNetworkList(getSUIDs = T)
-
-#lapply(SUID_list,tm_components)
-
-all_Suid.collection <- unique(sapply(SUID_list,getCollectionSuid))
-default_param <- sapply(getVisualPropertyNames(),getVisualPropertyDefault,"default")
-for (i in 1:length(all_Suid.collection)){
-  print(all_Suid.collection[i])
-  style.name  <- getCollectionName(all_Suid.collection[i])
-  n_i <- getCollectionNetworks(all_Suid.collection[i])[1]
-  nodeLabels <- mapVisualProperty('node label','id','p')
-  ind_coll <- grep(style.name,names(mm))
-  nodeFills <- mapVisualProperty(visual.prop = 'node fill color',table.column = 'scores',mapping.type = 'c',
-                                 table.column.values = as.numeric(c(mm[[ind_coll]][1],mean(mm[[ind_coll]]),mm[[ind_coll]][2])),
-                                 visual.prop.values = c('#FFF202','#FF7D00','#FC0000'))
-  createVisualStyle(style.name, default_param, list(nodeLabels,nodeFills))
-  lockNodeDimensions(FALSE, style.name)
-  # lapply(getCollectionNetworks(all_Suid.collection[i]),function(x){
-  #   setVisualStyle(style.name = style.name,network = x)})
-}

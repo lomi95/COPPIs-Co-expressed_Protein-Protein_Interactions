6f472f72f1579e0622420214f4c0b50c89835dab
lomi95 <67117720+lomi95@users.noreply.github.com>
2024-02-21 16:01:46 +0100

    Delete fun_confirmed directory

--- fun_confirmed/graph_from_correlation.R
+++
@@ -1,6 +0,0 @@
-graph_from_correlation <- function(cor_group,weights){
-  str_group   <- str_split(cor_group$cor_features,pattern = " and ", simplify = T)
-  graph_group <- graph_from_edgelist(cbind(str_group[,1],str_group[,2]), directed = F)
-  edge.attributes(graph_group)$weights <- weights
-  return(graph_group)
-}
